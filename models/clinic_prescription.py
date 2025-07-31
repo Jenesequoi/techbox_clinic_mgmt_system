036be68095f3c5b22c49a2362d8e0a358535217a
Julius-3367 <korirjulius001@gmail.com>
2025-07-31 11:37:39 +0300

    removed ClinicReception from clinic_prescription.py

--- models/clinic_prescription.py
+++ models/clinic_prescription.py
@@ -1,30 +1,5 @@
-# Filename: clinic_prescription.py
+
 from odoo import models, fields, api, _
-
-
-class ClinicReception(models.Model):
-    _name = 'clinic.reception'
-    _description = 'Clinic Reception'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
-    _order = 'date desc'
-
-    ref = fields.Char(string='Visit ID', required=True, copy=False)
-    date = fields.Date(string='Date', required=True, default=fields.Date.context_today)
-    patient_name = fields.Many2one('res.partner', string='Patient', required=True)
-    dob = fields.Date(string='Date of Birth', required=True)
-    age = fields.Integer(string='Age', store=True, tracking=True)
-    service = fields.Selection([
-        ('consultation', 'Consultation'),
-        ('mch', 'MCH'),
-        ('pharmacy', 'Pharmacy'),
-        ('laboratory', 'Laboratory'),
-        ('imaging', 'Imaging')
-    ], string='Outpatient Service', required=True)
-    is_referral = fields.Boolean(string='Is Referral', default=False)
-    referral_id = fields.Many2one('res.partner', string='Referral Doctor')
-    guardian = fields.Many2one('res.partner', string='Guardian')
-    is_married = fields.Boolean(string='Is Married', default=False)
-    spouse = fields.Many2one('res.partner', string='Spouse', tracking=True)
 
 
 class ClinicPrescription(models.Model):
